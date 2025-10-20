92338b6d99642ff52fd320e37af216e533d368a0
Anmol Singh <anmolshekhawat79@gmail.com>
2025-10-20 14:24:10 +0530

    Added Book Controller

---
+++ src/main/java/com/example/Bookify/Controller/BookController.java
@@ -0,0 +1,63 @@
+package com.example.Bookify.Controller;
+
+import com.example.Bookify.Entity.Book;
+import com.example.Bookify.Service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.Optional;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+
+    @Autowired
+    private BookService bookService;
+
+
+    @PostMapping
+    public Book addBook(@RequestBody Book book) {
+        return bookService.saveBookDetails(book);
+    }
+
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
+
+    // Get book by ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> getBookById(@PathVariable int id) {
+        Book book =   bookService.getBookById(id);
+         if(book !=  null)
+         {
+             return ResponseEntity.ok(book);
+         }
+         else
+         {
+             return  ResponseEntity.notFound().build();
+         }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Book> updateBook(@PathVariable int id, @RequestBody Book bookDetails) {
+        Book book =   bookService.updateBookById(id,bookDetails);
+        if(book !=  null)
+        {
+            return ResponseEntity.ok(book);
+        }
+        else
+        {
+            return  ResponseEntity.notFound().build();
+        }
+    }
+
+    // Delete a book by ID
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteBook(@PathVariable int id) {
+       return ResponseEntity.ok(bookService.deleteById(id));
+    }
+}
